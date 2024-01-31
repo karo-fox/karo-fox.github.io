470f833851a2f848ae927ab99c73c263512bec0c
Karolina Kozubik <k4aro0@gmail.com>
2024-01-31 14:35:25 +0100

    update navbar

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,6 @@
 function openMobileNav() {
-  const closedMenuClass = "flex flex-col items-end my-4 text-slate-400 md:hidden";
-  const openMenuClass = "hidden flex flex-col items-end my-4 text-slate-400";
+  const closedMenuClass = "grid gap-4 grid-cols-1 justify-items-end my-4 text-slate-400 md:hidden";
+  const openMenuClass = "hidden grid gap-4 grid-cols-1 justify-items-end my-4 text-slate-400";
   var menu = document.getElementById("menu");
   if (menu.className == openMenuClass) {
     menu.className = closedMenuClass;
