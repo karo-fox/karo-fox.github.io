646bd43e2a44a4a9a96776b2d24960cb73ac9993
Karolina Kozubik <k4aro0@gmail.com>
2024-01-31 12:34:21 +0100

    change color pallete

--- _site/assets/js/main.js
+++ _site/assets/js/main.js
@@ -1,6 +1,6 @@
 function openMobileNav() {
-  const closedMenuClass = "flex flex-col items-end my-4 text-cyan-500 md:hidden";
-  const openMenuClass = "hidden flex flex-col items-end my-4 text-cyan-500";
+  const closedMenuClass = "flex flex-col items-end my-4 text-slate-400 md:hidden";
+  const openMenuClass = "hidden flex flex-col items-end my-4 text-slate-400";
   var menu = document.getElementById("menu");
   if (menu.className == openMenuClass) {
     menu.className = closedMenuClass;
